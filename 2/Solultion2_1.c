7ba6fcea3b8258cc4c381435ffc81d6416876203
Nont <kunanont.nont@gmail.com>
2021-08-22 18:29:53 +0700

    edit

--- 2/Solultion2_1.c
+++ 2/Solultion2_1.c
@@ -1,12 +1,55 @@
+/*  
+    Task : Lab3 No.1
+    LANG : C
+    AUTHOR : Khunaon Maneesong 64010084
+*/
 #include <stdio.h>
+int Multiply(int x)
+{
+    int i=2,s=1,n[3]={0,0,0};
+    do 
+    {
+        n[i] = x%10;
+        s *= n[i];
+        x/=10;
+        i--;
+    }
+    while ( i >= 0 );
+    {
+        for (int i=0; i<3; i++)
+        {
+            if ( i == 2)
+            {
+                printf("%d = %d\n",n[i],s);
+            }
+            else printf("%d.",n[i]);
+        }
+        return s;
+    }
+}
+
+int Count_num(int y)
+{
+    int c=0;
+    do 
+    {
+        y/=10;
+        c++;
+    }
+    while (y>0);
+    {
+        return c;
+    }
+}
+
 int main()
 {
-    float BMI, w, h;
-    printf("Enter your Weight : ");
-    scanf("%f", &w);
-    printf("Enter your Height : ");
-    scanf("%f", &h);
-    BMI = w / (h * h);
-    printf("BMI = %.1f", BMI);
+    int n,sum,count;
+    scanf("%d",&n);
+    sum=Multiply(n);
+    while ( (count = Count_num(sum))>1 )
+    {
+        sum = Multiply(sum) ;
+    }
     return 0;
 }
