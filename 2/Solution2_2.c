1e766bc0509340701f4dd4ce7f02d71dad27a354
Nont <kunanont.nont@gmail.com>
2021-08-22 23:17:05 +0700

    up

--- 2/Solution2_2.c
+++ 2/Solution2_2.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
-
-
+#include <string.h>
+char x[3];
+char y[3];
 int main()
 {
-    float max=0;
-    float n1,n2,n3,num[3];
-    for (int i = 0; i <3; i++)
+    int sum = 1;
+    scanf("%s", x);
+    for (int i = 0; i < strlen(x); i++)
     {
-        scanf("%f",&num[i]);  
+        sum *= x[i] - '0';
     }
-    max=num[0];
-    for(int j=0;j<3;j++)
+    sprintf(y, "%d", sum);
+    printf("%c.%c.%c = %d\n",x[0],x[1],x[2],sum);
+    while (strlen(y) > 2)
     {
-        if (max<num[j])
-        {
-            max=num[j];
+        sum = 1;
+        for (int i = 0; i < strlen(y); i++)
+        { 
+            sum *= y[i] - '0';
+            if ( i == 2)
+            {
+                printf("%c = %d\n",y[i],sum);
+            }
+            else printf("%c.",y[i]);
         }
-        else {}
+        sprintf(y, "%d", sum);
+
     }
-    printf("The maximum is %G",max);
+    if (strlen(y) == 2)
+    {
+        printf("0.%c.%c = 0",y[0],y[1]);
+    }
     return 0;
 }
+/*
+    9*9*9 = 729
+    7*2*9 = 126
+    1*2*6 = 12
+    0*1*2 = 0
+*/
