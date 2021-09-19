d569b84ca8208b2b53398dc4922cbe2fa698429c
Nont <kunanont.nont@gmail.com>
2021-09-19 14:46:41 +0700

    up

---
+++ 6/Solution6_2.c
@@ -0,0 +1,18 @@
+/*  
+    Problem : รับค่าตัวเลข 2 จำนวน เก็บใน x และ y ตามลำดับ 
+              จากนั้นให้ทำการสลับค่ากัน โดยห้ามสร้างตัวแปรเพิ่ม (Level 5)
+    LANG : C
+    AUTHOR : Khunaon Maneesong 64010084
+*/
+#include <stdio.h>
+int main()
+{
+    int x, y;
+    printf("Before Swap : ");
+    scanf("%d %d",&x,&y);
+    x = x ^ y; 
+    y = x ^ y; 
+    x = x ^ y; 
+    printf("After Swap : %d %d", x, y);
+    return 0;
+}
